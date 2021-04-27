362a4b67aa2d63dc682c499cd8c5008eb8546e28
Sarah Desktop <sarahmantell8@gmail.com>
2021-04-27 09:34:58 -0700

    Resolved plotting issues. Cleaned up code.

--- plottingFunctions.py
+++ plottingFunctions.py
@@ -1,51 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from matplotlib.patches import Ellipse
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
 
-def plotMapping(real,imaginary):
-    theta = np.linspace(start=0, stop=2*math.pi, num=200)
-    x = np.cos(theta)
-    y = np.sin(theta)
+def plotEllipses(els,font=18, graphed = False, xlims = None, ylims=None, filename=None):
+    fig, ax = plt.subplots()
+    plt.rcParams.update({'font.size': font})
+    collection = PatchCollection(els, alpha=0.3,
+                                ec = 'darkblue', fc = 'cornflowerblue', lw = 2)
+    ax.add_collection(collection)
 
-    # Select length of axes and the space between tick labels
-    xmin, xmax, ymin, ymax = -10,10,-10,10
-    ticks_frequency = 1
-    # Plot points
-    fig, ax = plt.subplots(figsize=(5,5))
-    plt.scatter(real,imaginary,color="blue")
-    plt.scatter(x,y,color="red")
-
-    # Set identical scales for both axes
-    ax.set(xlim=(xmin-1, xmax+1), ylim=(ymin-1, ymax+1), aspect='equal')
-
+    
     # Set bottom and left spines as x and y axes of coordinate system
     ax.spines['bottom'].set_position('zero')
     ax.spines['left'].set_position('zero')
 
+    
     # Remove top and right spines
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
+
+    
     # Create 'x' and 'y' labels placed at the end of the axes
-    ax.set_xlabel('x', size=14, labelpad=-24, x=1.03)
-    ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+    ax.set_xlabel('x', size=font, labelpad=-24, x=1.03)
+    ax.set_ylabel('y', size=font, labelpad=-21, y=1.02, rotation=0)
+    
+    if graphed == True:    
+        # Draw major and minor grid lines
+        ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
 
-    # Create custom major ticks to determine position of tick labels
-    x_ticks = np.arange(xmin, xmax+1, ticks_frequency)
-    y_ticks = np.arange(ymin, ymax+1, ticks_frequency)
-    ax.set_xticks(x_ticks[x_ticks != 0])
-    ax.set_yticks(y_ticks[y_ticks != 0])
+    plt.axis("equal")
 
-    # Create minor ticks placed at each integer to enable drawing of minor grid
-    # lines: note that this has no effect in this example with ticks_frequency=1
-    ax.set_xticks(np.arange(xmin, xmax+1), minor=True)
-    ax.set_yticks(np.arange(ymin, ymax+1), minor=True)
+    if xlims is not None and ylims is not None:
+        plt.xlim(xlims[0],xlims[1])
+        plt.ylim(ylims[0],ylims[1])
 
-    # Draw major and minor grid lines
-    ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
-
-    # Draw arrows
-    arrow_fmt = dict(markersize=4, color='black', clip_on=False)
-    ax.plot((1), (0), marker='>', transform=ax.get_yaxis_transform(), **arrow_fmt)
-    ax.plot((0), (1), marker='^', transform=ax.get_xaxis_transform(), **arrow_fmt)
+    if filename is not None:
+        plt.savefig(filename,dpi=300)
 
     plt.show()
